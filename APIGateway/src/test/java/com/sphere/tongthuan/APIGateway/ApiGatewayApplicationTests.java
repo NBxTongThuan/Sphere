b4df54ea7ef795697db5be3c9d696a426e170ce0
TongThuan <tongthuan15092003@gmail.com>
2025-09-14 21:30:40 +0700

    Initial APIGateway

---
+++ APIGateway/src/test/java/com/sphere/tongthuan/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sphere.tongthuan.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
