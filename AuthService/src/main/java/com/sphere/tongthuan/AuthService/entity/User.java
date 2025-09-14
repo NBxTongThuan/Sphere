c8477e97f574fa6e6970c3106b5d07d0335859e9
TongThuan <tongthuan15092003@gmail.com>
2025-09-14 22:30:18 +0700

    change something

--- AuthService/src/main/java/com/sphere/tongthuan/AuthService/entity/User.java
+++ AuthService/src/main/java/com/sphere/tongthuan/AuthService/entity/User.java
@@ -1,8 +1,39 @@
 package com.sphere.tongthuan.AuthService.entity;
 
 
-public class User {
+import com.sphere.tongthuan.AuthService.constant.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userId;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserStatus status = UserStatus.ACTIVE;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now();
 
 
 }
